d4a315277e8200e28fa0b604e0f0cc75bdaf232f
Daniela Lotero <dlotero@wfscorp.com>
2017-12-11 12:41:59 -0500

    resolving this

---
+++ LinkedList/1-work-with-nodes-ex.js
@@ -0,0 +1,22 @@
+/*
+> we've created two nodes, Kitten and Puppy, and we've manually connected the Kitten node to the Puppy node.
+Create a Cat and Dog node and manually add them to the line.
+*/
+
+var Node = function(element){
+    this.element = element; 
+    this.next = null; 
+};
+var Kitten = new Node("Kitten");
+var Puppy = new Node("Puppy");
+
+Kitten.next = Puppy;
+// only add code below this line
+
+[Dog, Cat] = [new Node('Dog'), new Node('Cat')];
+
+Dog.next = Cat;
+Puppy.next = Dog;
+
+// test your code
+console.log(Kitten.next);
